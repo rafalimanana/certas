cca19ce737c195247a09f9fe70398ccbe6eaee2c
Fabrice <rafalimananafabrice@gmail.com>
2019-10-18 09:06:15 +0300

    home page

--- front-page.php
+++ front-page.php
@@ -4,7 +4,8 @@
  */
 
 $context = Timber::context();
-
+$args = 'category_name=actualite&numberposts=10';
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
+$context['articles'] = Timber::get_posts($args);
 Timber::render( array( 'front-page.twig' ), $context );
