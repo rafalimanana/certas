55f2fc6f30e824c2504371b50ef40fcba9163f14
Fabrice <rafalimananafabrice@gmail.com>
2019-10-21 15:32:23 +0300

    page

--- page.php
+++ page.php
@@ -23,6 +23,13 @@
 
 
 $context = Timber::context();
+$args = array(
+    'post_type' => 'post',
+    'order_by' => 'date',
+    'order' => 'DESC',
+    'post_status' => 'publish'
+);
+$context['posts'] = Timber::get_posts($args);
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
