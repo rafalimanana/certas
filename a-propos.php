2ade5c3c30d5a50d71d6eafe3124ccf1e89867c6
Fabrice <rafalimananafabrice@gmail.com>
2020-01-29 11:12:21 +0300

    A propos

--- a-propos.php
+++ a-propos.php
@@ -6,5 +6,6 @@
 $context = Timber::context();
 
 $timber_post = new Timber\Post();
+$context['page_contents'] = get_fields();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-a-propos.twig' ), $context ); 
