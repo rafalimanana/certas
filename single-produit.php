54f12d1c57fd6166501d9d9214eb57cc931883a4
Fabrice <rafalimananafabrice@gmail.com>
2020-02-05 09:05:49 +0300

    promos listes

---
+++ single-produit.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+global $post;
+
+$context = Timber::context();
+
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4,
+    'post__not_in' => array($post->ID),
+    'post_status' => 'publish',
+    'orderby' => array(
+		'date' => 'DESC'
+	)
+);
+
+$timber_post = Timber::query_post();
+$context['post'] = $timber_post;
+$context['posts'] = new Timber\PostQuery($args);
+
+if ( post_password_required( $timber_post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig' ), $context );
+}
---
+++ single-promo.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+global $post;
+
+$context = Timber::context();
+
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4,
+    'post__not_in' => array($post->ID),
+    'post_status' => 'publish',
+    'orderby' => array(
+		'date' => 'DESC'
+	)
+);
+
+
+$args_promos = array(
+    'post_type' => 'promo',
+    'posts_per_page' => 4,
+    'post__not_in' => array($post->ID),
+    'post_status' => 'publish',
+    'orderby' => array(
+		'date' => 'DESC'
+	)
+);
+
+$timber_post = Timber::query_post();
+$context['post'] = $timber_post;
+$context['posts'] = new Timber\PostQuery($args);
+$context['promos'] = new Timber\PostQuery($args_promos);
+
+if ( post_password_required( $timber_post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig' ), $context );
+}
