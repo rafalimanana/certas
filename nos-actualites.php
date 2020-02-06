1464c420be03b769720bf0c308fef933e51d1495
Fabrice <rafalimananafabrice@gmail.com>
2020-02-06 09:52:09 +0300

    modification

--- nos-actualites.php
+++ nos-actualites.php
@@ -3,32 +3,41 @@
  * Template Name: Page actualités
  */
 
+global $paged;
+if (!isset($paged) || !$paged){
+    $paged = 1;
+}
+
+
 $context = Timber::context();
 
 $l_args = array(
     'post_type' => 'post',
-    'order_by' => 'date',
-    'order' => 'DESC',
     'posts_per_page' => 1,
-    'category_name' => 'actualites',
-    'post_status' => 'publish'
+    // 'category_name' => 'actualites',
+    'post_status' => 'publish',
+    'orderby' => array(
+        'date' => 'DESC'
+    )
 );
-$last_posts = Timber::get_posts($l_args);
+$last_posts = new Timber\PostQuery($l_args);
 $last_id=0;
 if (!empty($last_posts)) {
 	$last_id = $last_posts[0]->ID;
 }
 $args = array(
     'post_type' => 'post',
-    'order_by' => 'date',
-    'order' => 'DESC',
-    'posts_per_page' => 6,
-    'category_name' => 'actualites',
+    'posts_per_page' => 11,
+    // 'category_name' => 'actualites',
     'post__not_in'      => array($last_id),
-    'post_status' => 'publish'
+    'post_status' => 'publish',
+    'paged' => $paged,
+    'orderby' => array(
+        'date' => 'DESC'
+    )
 );
 $context['last_posts'] = $last_posts;
-$context['posts'] = Timber::get_posts($args);
+$context['posts'] = new Timber\PostQuery($args);
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-nos-actualites.twig' ), $context );
